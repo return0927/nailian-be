52dbd888a2c00330f5d1bdd81a53f9d59cbc6af2
onehowon <flash0221@naver.com>
2025-03-15 22:20:59 +0900

    delete: 기존에 유지되던 추천 로직이 삭제

--- src/main/java/art/snail/naillian/backend/domain/nail/controller/NailSetController.java
+++ src/main/java/art/snail/naillian/backend/domain/nail/controller/NailSetController.java
@@ -1,10 +1,8 @@
 package art.snail.naillian.backend.domain.nail.controller;
 
 import art.snail.naillian.backend.common.CommonResponse;
-import art.snail.naillian.backend.domain.auth.jwt.UserAuthByTokenPayload;
 import art.snail.naillian.backend.domain.nail.dto.NailImageUrlDTO;
 import art.snail.naillian.backend.domain.nail.dto.NailSetEmbedDTO;
-import art.snail.naillian.backend.domain.nail.dto.NailSetRecommendationDTO;
 import art.snail.naillian.backend.domain.nail.service.NailService;
 import art.snail.naillian.backend.errors.ReportableError;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/nail-sets")
@@ -32,14 +28,6 @@ public class NailSetController {
                 .collectList()
                 .map(list -> new NailSetEmbedDTO<>(id, list))
                 .map(CommonResponse::success);
-    }
-
-    @GetMapping("/recommendations")
-    public Mono<CommonResponse<List<NailSetRecommendationDTO>>> getRecommendedNailSets(
-            UserAuthByTokenPayload payload
-    ) {
-        return nailService.getNailSetRecommendations(payload.getUserId())
-                .map(list -> CommonResponse.success(list, "추천 네일 세트 조회 성공"));
     }
 
     @GetMapping("/{id}/similar")
